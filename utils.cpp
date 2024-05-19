6dbb7e2443b18c348757e71645c9382ca84e281c
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-05-19 10:07:28 -0300

    utils for global search

--- utils.cpp
+++ utils.cpp
@@ -105,3 +105,96 @@ void generate_matrix(const std::vector<std::tuple<int, int, int>> &edges, std::v
         matrix[u][v] = w;
     }
 }
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+vector<int> get_permutations(int n, int index) {
+    vector<int> factorial(n, 1);
+    vector<int> permutation;
+    vector<int> elements;
+
+    for (int i = 1; i < n; i++) {
+        factorial[i] = factorial[i - 1] * i;
+        elements.push_back(i);
+    }
+    elements.push_back(n);
+
+    index--; // to convert to 0-based index
+    for (int i = n - 1; i >= 0; i--) {
+        int j = index / factorial[i];
+        index %= factorial[i];
+        permutation.push_back(elements[j]);
+        elements.erase(elements.begin() + j);
+    }
+
+    permutation.insert(permutation.begin(), 0);
+    permutation.push_back(0);
+
+    return permutation;
+}
+
+vector<vector<int>> valid_paths(vector<vector<int>> &paths, vector<vector<int>> &matrix, int total_capacity, map<int, int> &node_order) {
+    vector<vector<int>> possible_paths;
+    int n = paths.size();
+
+    for (int i = 0; i < n; i++) {
+        vector<int> new_path;
+        new_path.push_back(0); // Start with depot (0)
+        int total_capacity_used = 0;
+
+        for (int j = 0; j < paths[i].size() - 1; j++) {
+            int node = paths[i][j];
+            int next_node = paths[i][j + 1];
+
+            if (matrix[node][next_node] != -1 && total_capacity_used + node_order[next_node] <= total_capacity) {
+                new_path.push_back(next_node);
+                total_capacity_used += node_order[next_node];
+            } else {
+                if (new_path.back() != 0) { // Prevent consecutive zeros
+                    new_path.push_back(0);  // Return to depot to reset capacity
+                }
+                total_capacity_used = 0; // Reset capacity after returning to depot
+
+                if (matrix[0][next_node] != -1) { // If there is a path from depot to next_node
+                    new_path.push_back(next_node);
+                    total_capacity_used += node_order[next_node];
+                }
+            }
+        }
+
+        if (new_path.back() != 0) { // Avoid appending 0 if already at depot
+            new_path.push_back(0);
+        }
+
+        possible_paths.push_back(new_path);
+    }
+
+    return possible_paths;
+}
+
+vector<int> best_path(vector<vector<int>> &matrix, vector<vector<int>> &paths) {
+
+    vector<int> best_path;
+    int best_cost = INT_MAX;
+
+    for (int i = 0; i < paths.size(); i++) {
+        vector<int> path = paths[i];
+        int cost = 0;
+        for (int j = 0; j < path.size() - 1; j++) {
+            int node = path[j];
+            int next_node = path[j + 1];
+            cost += matrix[node][next_node];
+        }
+        cout << "Cost of path " << i << ": " << cost << endl;
+        if (cost < best_cost) {
+            best_cost = cost;
+            best_path = path;
+        }
+    }
+
+    return best_path;
+}
--- utils.h
+++ utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <algorithm>
+#include <climits>
 #include <fstream>
 #include <iostream>
 #include <iterator> // For advance
@@ -19,4 +20,8 @@ typedef std::tuple<int, int, int> Edge; // Define a type alias for the edge tupl
 // Function declaration for reading the graph from a file
 void read_graph(const std::string filename, std::vector<Edge> *edges, std::map<int, int> *node_order);
 void generate_matrix(const std::vector<std::tuple<int, int, int>> &edges, std::vector<std::vector<int>> &matrix);
+int factorial(int n);
+std::vector<int> get_permutations(int n, int index);
+std::vector<std::vector<int>> valid_paths(std::vector<std::vector<int>> &paths, std::vector<std::vector<int>> &matrix, int total_capacity, std::map<int, int> &node_order);
+std::vector<int> best_path(std::vector<std::vector<int>> &matrix, std::vector<std::vector<int>> &paths);
 #endif // UTILS_H
